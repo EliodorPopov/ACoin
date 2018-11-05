ce6618649ced4f14aa3f806ce9fdc7e5d0cf2546
FluffyK <andrei.lupascu@ati.utm.md>
2018-11-05 19:19:07 +0200

    Add Expenses and Earnings page(MVP)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-//import 'package:acoin/modal.dart';
+
+import 'package:acoin/modal.dart';
 import 'package:flutter/material.dart';
+
 
 void main() => runApp(new MyApp());
 
@@ -8,22 +10,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'ACoin',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+      theme:  ThemeData.light().copyWith(
+          inputDecorationTheme:
+              InputDecorationTheme(border: OutlineInputBorder())
       ),
       home: new MyHomePage(title: 'ACoin'),
-      routes:<String, WidgetBuilder>{
-        "/EarningsPage": (BuildContext context) => new EarningsPage()
-      } ,
-    );
+         );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
+  
   final String title;
-
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
@@ -48,78 +47,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: new FloatingActionButton(
-        onPressed: () => modal.mainBottomSheet(context),
+        onPressed: () =>  modal.mainBottomSheet(context),
         child: new Icon(Icons.add),
       ),
     );
   }
 }
 
-class EarningsPage extends StatefulWidget {
-  EarningsPage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _EarningsPageState createState() => new _EarningsPageState();
-}
-
-class _EarningsPageState extends State<EarningsPage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          
-        ),
-      ),
-      
-    );
-  }
-}
-
-class Modal{
-  mainBottomSheet(BuildContext context){
-    showModalBottomSheet(
-        context: context,
-        builder: (BuildContext context){
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              _createTile(context, 'Add Earnings', Icons.monetization_on, _action1),
-              _createTile(context, 'Add Expenses', Icons.money_off, _action2),
-              _createTile(context, 'View Graphs', Icons.graphic_eq, _action3),
-            ],
-          );
-        }
-    );
-  }
-
-  ListTile _createTile(BuildContext context, String name, IconData icon, Function action){
-    return ListTile(
-      leading: Icon(icon),
-      title: Text(name),
-      onTap: (){
-        Navigator.pop(context);
-        Navigator.of(context).pushNamed("/EarningsPage");
-        action();
-      },
-    );
-  }
-
-  _action1(){
-    print('action 1');
-  }
-
-  _action2(){
-    print('action 2');
-  }
-
-  _action3(){
-    print('action 3');
-  }
-}
