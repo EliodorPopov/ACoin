81e62bbf828d216f3ecdf85d9747ee6331f71095
xandru1 <xandru1@github.com>
2018-12-06 20:06:52 +0200

    validare si goals banner

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:acoin/splashScr.dart';
+import 'package:flutter/material.dart';
+//import 'buildExpensesPage.dart';
+
+void main() => runApp(
+      new MaterialApp(
+        home: new SplashScr(),
+      ),
+    );
