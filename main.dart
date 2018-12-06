68c4d4b880a34a3e29350e19b6799dc48e234cd5
xandru1 <xandru1@github.com>
2018-12-06 20:32:21 +0200

    deleted everything

--- main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:acoin/splashScr.dart';
-import 'package:flutter/material.dart';
-//import 'buildExpensesPage.dart';
-
-void main() => runApp(
-      new MaterialApp(
-        home: new SplashScr(),
-      ),
-    );
